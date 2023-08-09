02f45c21f0b0b4932caa435a07fe997a1c6e1e29
natrix1 <141676108+natrix1@users.noreply.github.com>
2023-08-09 16:22:13 -0300

    hice un objeto
    agregue constructo y genere en el main

--- src/practicagithub/Pract1.java
+++ src/practicagithub/Pract1.java
@@ -13,4 +13,35 @@ public class Pract1 {
     private String nombre;
     private String edad;
     private int votos;
+
+    public Pract1(String nombre, String edad, int votos) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.votos = votos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+    
 }
